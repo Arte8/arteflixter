a46d00216d03bd1b99c19919b176cbed650f2b08
Art <artelo.studio@gmail.com>
2019-04-28 20:50:59 +0000

    added modal troubleshoot err

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,15 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-  resources :courses, only: [:index, :show]
+  # resources :courses, only: [:index, :show]
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+    resources :sections, only: [:update] do
+      # resources :lessons, only: [:new, :create]
     end
-    resources :sections, only: []
     resources :courses, only: [:new, :create, :show] do
+    # resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
     end
