f8529fecfa5bc96110172fc27c29a49be4a4b81d
Art <artelo.studio@gmail.com>
2019-04-28 19:43:10 +0000

    L35 err

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,14 +1,18 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
-  def new
-    @section = Section.new
+  
+    
+   def create
+     @section = current_course.sections.create(section_params)
+     redirect_to instructor_course_path(current_course)
   end
 
-  def create
-    @section = current_course.sections.create(section_params)
-    redirect_to instructor_course_path(current_course)
+    def update
+    current_section.update_attributes(section_params)
+    render plain: 'ok!'
   end
 
   private
@@ -19,14 +23,24 @@ class Instructor::SectionsController < ApplicationController
     end
   end
 
-  helper_method :current_course
-def current_course
-  if params[:course_id]
-    @current_course ||= Course.find(params[:course_id])
-  else
-    current_section.course
+  def current_section
+    @current_section ||= Section.find(params[:id])
   end
-end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+   helper_method :current_course
+ def current_course
+   # if params[:course_id]
+    @current_course ||= Course.find(params[:course_id])
+   end
+  #    current_section.course
+  # end
+
 
   def section_params
     params.require(:section).permit(:title)
