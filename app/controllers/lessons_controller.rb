9f7846e42c8f181235c8bd1fd04e5f96c574694d
Art <artelo.studio@gmail.com>
2019-05-01 04:05:19 +0000

    flixter end trouble shoot

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,5 @@
 class LessonsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  before_action :authenticate_user!
+#  before_action :authenticate_user!
   before_action :require_authorized_for_current_lesson, only: [:show]
     
 
